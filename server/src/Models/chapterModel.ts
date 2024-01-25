7f1c9312fc70519f5a84161e694c6020f126b8c6
Joey Cox <wesnat20@gmail.com>
2024-01-24 18:35:59 -0700

    Created Chapter Schema/Model

---
+++ server/src/Models/chapterModel.ts
@@ -0,0 +1,115 @@
+import mongoose, { Document } from "mongoose";
+
+export interface ChapterI extends Document {
+  name: string;
+  chapterNumber: number;
+  storyId: mongoose.Schema.Types.ObjectId;
+  userId: mongoose.Schema.Types.ObjectId;
+  chapterContent: string;
+  chapterContentWords: string;
+  wordcount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  needEditing: boolean;
+}
+
+const ChapterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "You must provide a chapter name"],
+  },
+  chapterNumber: {
+    type: Number,
+    default: 1,
+  },
+  storyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "StoryModel",
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "UserModel",
+    required: true,
+  },
+  needEditing: {
+    type: Boolean,
+    default: false,
+  },
+  chapterContent: {
+    type: String,
+    required: [true, "You must provide content to the chapter"],
+  },
+  chapterContentWords: {
+    type: String,
+    required: [true, "You must provide content to the chapter"],
+  },
+  wordCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (date: any) => {
+      let seconds = Math.floor((new Date().getTime() - date) / 1000);
+      let interval = seconds / 31536000;
+
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return date.toLocaleDateString("en-US");
+      }
+
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " Days Ago";
+      }
+
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " Hours Ago";
+      }
+
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " Minutes Ago";
+      }
+
+      return "1 Minute Ago";
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    get: (date: any) => {
+      let seconds = Math.floor((new Date().getTime() - date) / 1000);
+      let interval = seconds / 31536000;
+
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return date.toLocaleDateString("en-US");
+      }
+
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " Days Ago";
+      }
+
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " Hours Ago";
+      }
+
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " Minutes Ago";
+      }
+
+      return "1 Minute Ago";
+    },
+  },
+});
+
+const ChapterModel = mongoose.model("ChapterModel", ChapterSchema);
+
+export default ChapterModel;
