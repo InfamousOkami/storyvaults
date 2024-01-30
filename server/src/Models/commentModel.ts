13703580310505b70b6662828a1687b9b66ab411
Joey Cox <wesnat20@gmail.com>
2024-01-29 17:34:32 -0700

    Created comment Schema

---
+++ server/src/Models/commentModel.ts
@@ -0,0 +1,102 @@
+import mongoose from "mongoose";
+
+type CommentTypeT = "chapter" | "story" | "comment";
+
+export interface CommentI extends Document {
+  commentType: CommentTypeT;
+  userId: mongoose.Schema.Types.ObjectId;
+  parentId: string;
+  commentContent: string;
+  likes: Map<mongoose.Schema.Types.ObjectId, boolean>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CommentSchema = new mongoose.Schema({
+  commentType: {
+    type: String,
+    enum: ["chapter", "story", "comment"],
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "UserModel",
+    required: true,
+  },
+  parentId: {
+    type: String,
+    required: true,
+  },
+  commentContent: {
+    type: String,
+    required: [true, "Must provide the comment"],
+  },
+  likes: {
+    type: Map,
+    of: Boolean,
+    default: new Map(),
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (date: any) => {
+      let seconds = Math.floor((new Date().getTime() - date) / 1000);
+      let interval = seconds / 31536000;
+
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return date.toLocaleDateString("en-US");
+      }
+
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " Days Ago";
+      }
+
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " Hours Ago";
+      }
+
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " Minutes Ago";
+      }
+
+      return "1 Minute Ago";
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    get: (date: any) => {
+      let seconds = Math.floor((new Date().getTime() - date) / 1000);
+      let interval = seconds / 31536000;
+
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return date.toLocaleDateString("en-US");
+      }
+
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " Days Ago";
+      }
+
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " Hours Ago";
+      }
+
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " Minutes Ago";
+      }
+
+      return "1 Minute Ago";
+    },
+  },
+});
+
+const CommentModel = mongoose.model("CommentModel", CommentSchema);
+
+export default CommentModel;
