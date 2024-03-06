0a8628bad99846172e7af63f31c0b7a1ba5d21bf
Joey Cox <wesnat20@gmail.com>
2024-03-06 16:34:28 -0700

    Updated Filter to add NSFW & Updated Breaker

--- client/src/components/breaker/Breaker.tsx
+++ client/src/components/breaker/Breaker.tsx
@@ -1,13 +1,13 @@
 function Breaker({ type }: { type: string }) {
   return (
     <>
-      {type === "under" ? (
-        <div className="bg-gray-700 h-[1px] w-[99%] m-auto" />
+      {type === 'under' ? (
+        <div className="m-auto h-[1px] w-[99%] bg-gray-700" />
       ) : (
-        <div className="bg-gray-700 h-[1px] w-[6px] my-auto" />
+        <div className="text-gray-700">-</div>
       )}
     </>
-  );
+  )
 }
 
-export default Breaker;
+export default Breaker
--- client/src/components/filter/Filter.tsx
+++ client/src/components/filter/Filter.tsx
@@ -1,163 +1,173 @@
-"use client";
+'use client'
 
-import axios from "axios";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import axios from 'axios'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import LoadingPulse from "../loading/LoadingSpinner";
-import { CategoryI, GenreI, LanguageI } from "@/typings";
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import LoadingPulse from '../loading/LoadingSpinner'
+import { CategoryI, GenreI, LanguageI } from '@/typings'
 
 const getCategoryName = (type: string) => {
   switch (type) {
-    case "oneShot":
-      return "One-Shots";
+    case 'shortStory':
+      return 'Short Story'
 
-    case "book":
-      return "Books";
+    case 'book':
+      return 'Books'
 
-    case "lightNovel":
-      return "Light Novels";
+    case 'lightNovel':
+      return 'Light Novels'
 
     default:
-      return "Unidentified";
+      return 'Unidentified'
   }
-};
+}
 
 function Filter() {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams()
 
   // Load Use States
-  const [genres, setGenres] = useState([]);
-  const [languages, setLanguages] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [genres, setGenres] = useState([])
+  const [languages, setLanguages] = useState([])
+  const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   // Params Use States
   const [keywords, setKeywords] = useState(
-    searchParams.has("keywords") ? searchParams.get("keywords") ?? "" : ""
-  );
+    searchParams.has('keywords') ? searchParams.get('keywords') ?? '' : ''
+  )
 
   const [readersAccess, setReadersAccess] = useState(
-    searchParams.has("readersAccess") ? searchParams.get("readersAccess") : ""
-  );
+    searchParams.has('readersAccess') ? searchParams.get('readersAccess') : ''
+  )
 
   const [sortOrder, setSortOrder] = useState(
-    searchParams.has("readersAccess")
-      ? searchParams.get("readersAccess")
-      : "desc"
-  );
+    searchParams.has('readersAccess')
+      ? searchParams.get('readersAccess')
+      : 'desc'
+  )
 
   const [sortBy, setSortBy] = useState(
-    searchParams.has("sort") ? searchParams.get("sort") : "updatedAt"
-  );
+    searchParams.has('sort') ? searchParams.get('sort') : 'updatedAt'
+  )
 
   const [category, setCategory] = useState(
-    searchParams.has("category") ? searchParams.get("category") : ""
-  );
+    searchParams.has('category') ? searchParams.get('category') : ''
+  )
 
   const [genre, setGenre] = useState(
-    searchParams.has("genre") ? searchParams.get("genre") : ""
-  );
+    searchParams.has('genre') ? searchParams.get('genre') : ''
+  )
 
   const [language, setLanguage] = useState(
-    searchParams.has("languageName") ? searchParams.get("languageName") : ""
-  );
+    searchParams.has('languageName') ? searchParams.get('languageName') : ''
+  )
 
   const [status, setStatus] = useState(
-    searchParams.has("status") ? searchParams.get("status") : ""
-  );
+    searchParams.has('status') ? searchParams.get('status') : ''
+  )
+
+  const [nsfw, setNsfw] = useState(
+    searchParams.has('nsfw') ? searchParams.get('nsfw') : 'false'
+  )
 
   const [limit, setLimit] = useState(
-    searchParams.has("limit") ? searchParams.get("limit") : "30"
-  );
+    searchParams.has('limit') ? searchParams.get('limit') : '30'
+  )
 
   const getData = async () => {
-    const genres = await axios.get(`http://localhost:8080/api/v1/genre`);
-    setGenres(genres.data.data);
+    const genres = await axios.get(`http://localhost:8080/api/v1/genre`)
+    setGenres(genres.data.data)
 
-    const languages = await axios.get(`http://localhost:8080/api/v1/language`);
-    setLanguages(languages.data.data);
+    const languages = await axios.get(`http://localhost:8080/api/v1/language`)
+    setLanguages(languages.data.data)
 
-    const categories = await axios.get(`http://localhost:8080/api/v1/category`);
-    setCategories(categories.data.data);
+    const categories = await axios.get(`http://localhost:8080/api/v1/category`)
+    setCategories(categories.data.data)
 
-    setIsLoading(false);
-  };
+    setIsLoading(false)
+  }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
-  const router = useRouter();
-  const pathname = usePathname();
+  const router = useRouter()
+  const pathname = usePathname()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const params = new URLSearchParams(searchParams.toString());
+    e.preventDefault()
+    const params = new URLSearchParams(searchParams.toString())
 
     if (keywords) {
-      params.set("keywords", keywords!);
+      params.set('keywords', keywords!)
     } else {
-      params.delete("keywords");
+      params.delete('keywords')
     }
-    params.set("sortOrder", sortOrder!);
-    params.set("sort", sortBy!);
-    params.set("limit", limit!);
-    params.set("page", "1");
+    params.set('sortOrder', sortOrder!)
+    params.set('sort', sortBy!)
+    params.set('limit', limit!)
+    params.set('page', '1')
 
     if (category) {
-      params.set("category", category);
+      params.set('category', category)
     } else {
-      params.delete("category");
+      params.delete('category')
     }
 
     if (genre) {
-      params.set("genre", genre);
+      params.set('genre', genre)
     } else {
-      params.delete("genre");
+      params.delete('genre')
     }
 
     if (language) {
-      params.set("languageName", language);
+      params.set('languageName', language)
     } else {
-      params.delete("languageName");
+      params.delete('languageName')
     }
 
     if (status) {
-      params.set("status", status);
+      params.set('status', status)
     } else {
-      params.delete("status");
+      params.delete('status')
+    }
+
+    if (nsfw) {
+      params.set('nsfw', nsfw)
+    } else {
+      params.delete('nsfw')
     }
 
     if (readersAccess) {
-      params.set("readerAccess", readersAccess!);
+      params.set('readerAccess', readersAccess!)
     } else {
-      params.delete("readerAccess");
+      params.delete('readerAccess')
     }
 
-    router.push(pathname + "?" + params.toString());
-  };
+    router.push(pathname + '?' + params.toString())
+  }
 
-  if (isLoading) <LoadingPulse />;
+  if (isLoading) <LoadingPulse />
 
   return (
-    <div className="mb-5 pt-5 w-[98%] md:max-w-[600px] mx-auto">
+    <div className="mx-auto mb-5 w-[98%] pt-5 md:max-w-[600px]">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col gap-3 items-center "
+        className="flex flex-col items-center gap-3 "
       >
         {/* keywords, sort order, sort by */}
-        <div className="flex flex-col gap-3 items-center md:flex-row">
+        <div className="flex flex-col items-center gap-3 md:flex-row">
           {/* Keyword Search */}
           <label htmlFor="keywords" />
           <input
-            className="border w-full md:w-56 py-1 px-1 border-black rounded-md"
+            className="w-full rounded-md border border-black px-1 py-1 md:w-56"
             type="text"
             placeholder="Search..."
             name="keywords"
             value={keywords}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setKeywords(e.target.value);
+              setKeywords(e.target.value)
             }}
           />
 
@@ -171,14 +181,14 @@ function Filter() {
                   type="radio"
                   name="sortOrder"
                   value="desc"
-                  checked={sortOrder === "desc"}
-                  onChange={() => setSortOrder("desc")}
+                  checked={sortOrder === 'desc'}
+                  onChange={() => setSortOrder('desc')}
                   hidden
                 />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className={`icon icon-tabler icon-tabler-arrow-narrow-down ${
-                    sortOrder === "desc" ? "text-blue-600" : "text-gray-700"
+                    sortOrder === 'desc' ? 'text-blue-600' : 'text-gray-700'
                   }`}
                   width="24"
                   height="24"
@@ -200,14 +210,14 @@ function Filter() {
                   type="radio"
                   name="sortOrder"
                   value="asc"
-                  checked={sortOrder === "asc"}
-                  onChange={() => setSortOrder("asc")}
+                  checked={sortOrder === 'asc'}
+                  onChange={() => setSortOrder('asc')}
                   hidden
                 />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className={`icon icon-tabler icon-tabler-arrow-narrow-up ${
-                    sortOrder === "asc" ? "text-blue-600" : "text-gray-700"
+                    sortOrder === 'asc' ? 'text-blue-600' : 'text-gray-700'
                   }`}
                   width="24"
                   height="24"
@@ -228,11 +238,12 @@ function Filter() {
 
             {/* SortBy */}
             <div className="flex gap-1">
-              <label htmlFor="readerAccess">Sort By</label>
+              <label htmlFor="sortBy">Sort By</label>
               <select
-                name="readerAccess"
+                name="sortBy"
                 id=""
                 className=""
+                value={sortBy as string}
                 onChange={(e: FormEvent<HTMLSelectElement>) =>
                   setSortBy((e.target as HTMLSelectElement).value)
                 }
@@ -251,13 +262,14 @@ function Filter() {
         </div>
 
         {/* Category, Genre */}
-        <div className="w-full flex gap-1">
+        <div className="flex w-full gap-1">
           {/* Category */}
           <label htmlFor="category">Category</label>
           <select
             name="category"
             id=""
             className="flex-1"
+            value={category as string}
             onChange={(e: FormEvent<HTMLSelectElement>) =>
               setCategory((e.target as HTMLSelectElement).value)
             }
@@ -276,6 +288,7 @@ function Filter() {
             name="genre"
             id=""
             className="flex-1"
+            value={genre as string}
             onChange={(e: FormEvent<HTMLSelectElement>) =>
               setGenre((e.target as HTMLSelectElement).value)
             }
@@ -297,6 +310,7 @@ function Filter() {
             name="language"
             id=""
             className="flex-1"
+            value={language as string}
             onChange={(e: FormEvent<HTMLSelectElement>) =>
               setLanguage((e.target as HTMLSelectElement).value)
             }
@@ -315,6 +329,7 @@ function Filter() {
             name="readerAccess"
             id=""
             className="flex-1"
+            value={readersAccess as string}
             onChange={(e: FormEvent<HTMLSelectElement>) =>
               setReadersAccess((e.target as HTMLSelectElement).value)
             }
@@ -326,6 +341,7 @@ function Filter() {
           </select>
         </div>
 
+        {/* Status & NSFW */}
         <div className="flex gap-1 self-center">
           {/* Status */}
           <label htmlFor="status">Status</label>
@@ -333,6 +349,7 @@ function Filter() {
             name="status"
             id=""
             className=""
+            value={status as string}
             onChange={(e: FormEvent<HTMLSelectElement>) =>
               setStatus((e.target as HTMLSelectElement).value)
             }
@@ -341,7 +358,24 @@ function Filter() {
             <option value="Incomplete">Incomplete</option>
             <option value="Complete">Complete</option>
           </select>
-          {/* Status */}
+
+          {/* NSFW */}
+          <label htmlFor="nsfw">NSFW</label>
+          <select
+            name="nsfw"
+            id=""
+            className=""
+            value={nsfw as string}
+            onChange={(e: FormEvent<HTMLSelectElement>) =>
+              setNsfw((e.target as HTMLSelectElement).value)
+            }
+          >
+            <option value="">All</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+
+          {/* limit */}
           <label htmlFor="limit">Per Page</label>
           <select
             name="limit"
@@ -359,14 +393,14 @@ function Filter() {
 
         {/* Submit Button */}
         <button
-          className="bg-blue-500 py-2 text-white hover:bg-blue-700 w-56 rounded-lg self-center"
+          className="w-56 self-center rounded-lg bg-blue-500 py-2 text-white hover:bg-blue-700"
           type="submit"
         >
           Submit
         </button>
       </form>
     </div>
-  );
+  )
 }
 
-export default Filter;
+export default Filter
